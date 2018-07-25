3486f45fdf0b85949b18515a7caf046e6a477640
邹志鹏 <lavi@zouzhipengdeMacBook-Pro.local>
2018-07-26 00:52:38 +0800

    init project

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8080);
+  process.stdout.write('your application is running at localhost:8080');
+}
+bootstrap();
