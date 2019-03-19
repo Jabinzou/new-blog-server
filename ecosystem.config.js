2a37e37977083ebb842b3198f8d4120945c3d414
jabinzou <jabinzou1@gmail.com>
2019-03-19 13:33:33 +0800

    :zap: 优化项目启动选项
    优化项目启动选项

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,7 @@ module.exports = {
   apps: [{
     name: 'blog_server',
     script: './dist/main.js',                      // 执行文件
-    cwd: './',                                // 根目录
-    args: './tsconfig-paths-bootstrap.js',
-    interpreter: 'node', // 解释器
-    interpreter_args: '-r', // 解释器参数
+    cwd: './',                           
     min_uptime: 10000, // 启动10s以内挂掉的进程视为异常重启，超过默认最大重启次数15次之后即不再自动重启
     exec_mode: 'cluster',
     instances: process.env.INSTANCES || Math.floor(os.cpus().length * 0.75),
